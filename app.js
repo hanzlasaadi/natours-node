f9b2eb95073741758d01ff8b3151fa21002e3a98
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-03 16:13:06 +0500

    post method

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const port = 6969;
 
 app.get('/', (req, res) => {
-  console.log(req.url)
-  res.status(200).send({ message: 'HELLOOO, its me...', name: 'Natours by Hanzla'});
-})
+  console.log(req.url);
+  res
+    .status(200)
+    .send({ message: 'HELLOOO, its me...', name: 'Natours by Hanzla' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('You just posted on the mfucking SERVER.');
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port} and being HORNY...`)
-})
+  console.log(`Listening on port ${port} and being HORNY...`);
+});

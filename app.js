22d98d7c51292f00f8b5facce3c4cbb65985ce83
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-04 05:11:53 +0500

    GET: Tours API

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 6969;
 
-app.get('/', (req, res) => {
-  console.log(req.url);
-  res
-    .status(200)
-    .send({ message: 'HELLOOO, its me...', name: 'Natours by Hanzla' });
-});
+// app.get('/', (req, res) => {
+//   console.log(req.url);
+//   res
+//     .status(200)
+//     .send({ message: 'HELLOOO, its me...', name: 'Natours by Hanzla' });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).send('You just posted on the mfucking SERVER.');
+// app.post('/', (req, res) => {
+//   console.log(req.url);
+//   res.status(200).send('You just posted on the mfucking SERVER.');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  console.log(req.url);
+  res.send({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 app.listen(port, () => {

dfbb305ffd12d8511ae674966d1d25f55c379def
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-04 05:49:05 +0500

    POST: add new tour

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
+app.use(express.json()); //MIDDLEWARE: stores data between request & response
+
 const port = 6969;
 
 // app.get('/', (req, res) => {
@@ -28,6 +31,31 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.url);
+  console.log(req.body);
+
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign(req.body, { id: newID }); //MErge new ID & requested tour objects
+
+  tours.push(newTour); //add newly requested tour to main Tour Object
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+  // res.send('POSTed on the server');
 });
 
 app.listen(port, () => {

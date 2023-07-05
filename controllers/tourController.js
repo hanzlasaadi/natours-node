2836a76d347ad6de2de79bd412ebc791db887a36
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-05 13:31:49 +0500

    POSTing docs & GETTing docs by async/await

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const Tour = require('./../models/tourModel');
 
 //CheckBody Middleware
+// eslint-disable-next-line consistent-return
 exports.checkBody = (req, res, next) => {
   console.log('Checkbody middleware is working');
   if (!req.body)
@@ -18,38 +19,59 @@ exports.checkBody = (req, res, next) => {
 };
 
 //------ROUTE HANDLERS/CONTROLLERS------
-exports.getAllTours = (req, res) => {
-  // console.log(req.url);
-  res.send({
-    status: 'success'
-    // results: tours.length,
-    // data: {
-    //   tours: tours
-    // }
-  });
+exports.getAllTours = async function(req, res) {
+  try {
+    const tours = await Tour.find();
+    res.send({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours: tours
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
-exports.getATour = (req, res) => {
-  // const id = req.params.id * 1;
-  // const tour = tours.find(val => val.id === id);
-
-  res.status(200).send({
-    status: 'success',
-    requestedTime: req.requestedTime
-    // data: {
-    //   tour: tour
-    // }
-  });
+exports.getATour = async function(req, res) {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.send({
+      status: 'success',
+      data: {
+        tours: tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
-exports.addNewTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // results: tours.length,
-    // data: {
-    //   tour: newTour
-    // }
-  });
+// exports.addNewTour = (req, res) => async {
+exports.addNewTour = async function(req, res) {
+  try {
+    // const newTour = new Tour();
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {

038593fa8eeee96ce593872c520bf0d88ef56581
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-05 11:59:44 +0500

    making docs/collections & testing model

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+// console.log(process.env);
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
@@ -16,7 +17,38 @@ mongoose
     console.log('DB connection sucessfull!!!');
   });
 
-// console.log(process.env);
+// Describing mongoose schema
+const tourSchema = mongoose.Schema({
+  name: {
+    unique: true,
+    type: String,
+    required: [true, 'A tour must have a name e.g., Hunza Valley']
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price tag e.g., 69.9$']
+  },
+  rating: {
+    type: Number,
+    default: 6.9
+  }
+});
+
+//Making a model from mongoose schema
+const Tour = mongoose.model('Tour', tourSchema);
+
+//Creating an instance of the tour MODEL created above👆 with data
+const testTour = new Tour({
+  name: 'Harvey Specter',
+  price: 129,
+  rating: 7.5
+});
+
+// Saving the model instance in the database as a DOCUMENT📄
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log('💣💣Error happened💣💣: ', err.message));
 
 const port = process.env.PORT || 6969;
 app.listen(port, () => {

4f2020a1201b8bf63fced1b6f172ad3ee0343b32
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-05 07:13:53 +0500

    Environment Variables

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 6969;
+console.log(process.env);
+
+const port = process.env.PORT || 6969;
 app.listen(port, () => {
   console.log(`Listening on port ${port} and being HORNY...`);
 });

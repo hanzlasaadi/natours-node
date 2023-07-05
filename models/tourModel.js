39a9b47be50060e1314132cc49e748abc72d87f8
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-05 14:40:03 +0500

    DB schema updated

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,16 +6,56 @@ const tourSchema = mongoose.Schema({
   name: {
     unique: true,
     type: String,
-    required: [true, 'A tour must have a name e.g., Hunza Valley']
+    required: [true, 'A tour must have a name e.g., Hunza Valley'],
+    trim: true
   },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration! (a number)']
+  },
+  difficulty: {
+    type: String,
+    required: [
+      true,
+      'A tour must have a difficulty level! (easy, medium, hard)'
+    ]
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Must define a good enough group size!']
+  },
+  summary: {
+    type: String,
+    required: [true, 'You must provide a summary of the tour!'],
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  priceDiscount: Number,
+  ratingsAvg: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'must have a cover image']
+  },
+  images: [String],
   price: {
     type: Number,
     required: [true, 'A tour must have a price tag e.g., 69.9$']
   },
-  rating: {
-    type: Number,
-    default: 6.9
-  }
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  startDates: [Date]
 });
 
 //Making a model from mongoose schema

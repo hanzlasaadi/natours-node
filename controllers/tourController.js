cd8a7a37157ba7f58a2b89180a40a1bc52bb5f0c
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-06 10:50:41 +0500

    Limiting result fields & Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -50,6 +50,26 @@ exports.getAllTours = async function(req, res) {
       query = query.sort('-createdAt');
     }
 
+    // 3) Limiting Fields in the Results
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 5;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This many tours do not exist');
+    }
+
     // Executing Query;
     const tours = await query;
 

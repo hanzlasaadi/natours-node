3ecb1b496c6f98b683a8497c3991dacc1a1eecf5
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-08 19:28:55 +0500

    Password confirm validate & encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+const bcrypt = require('bcryptjs');
 const validation = require('validator');
 const mongoose = require('mongoose');
 
@@ -25,8 +26,23 @@ const userSchema = mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Confirm your passwords']
+    required: [true, 'Confirm your passwords'],
+    validate: {
+      validator: function(val) {
+        return val === this.password;
+      },
+      message: 'Password must match! Try Again!!!'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  return next();
 });
 
 const User = mongoose.model('User', userSchema);

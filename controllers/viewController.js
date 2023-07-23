3c5500746b813848b569c063fadd5872feb37999
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-24 03:01:43 +0500

    signup with api (1)

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -49,7 +49,13 @@ exports.login = catchAsync(async (req, res) => {
 });
 
 exports.signup = catchAsync(async (req, res) => {
-  res.status(200).render('signup', {
-    title: 'Create new account'
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.cloudfare.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://cdnjs.cloudfare.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('signup', {
+      title: 'Create new account'
+    });
 });
---
+++ public/js/signupForm.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:6969/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    });
+  } catch (err) {
+    console.log(err.response.data);
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', async e => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const passwordConfirm = document.getElementById('passwordConfirm').value;
+  signup(name, email, password, passwordConfirm);
+});

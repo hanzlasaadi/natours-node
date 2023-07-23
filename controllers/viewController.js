6b0d77891d21611e4abe8601574899924fcb7d2f
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-23 15:49:21 +0500

    tour overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1. get tours data from database
+  const tours = await Tour.find();
+
+  // 2. build the template - ✅ at 'overview.pug'
+
+  // 3. render the final template with the data
   res.status(200).render('overview', {
-    title: 'All the tours'
+    title: 'All the tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
